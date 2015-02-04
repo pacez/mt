4ecc5763b79e65341c25f867678941654905a80d
Pace Zhong <pace_zhong@foxmail.com>
2015-02-04 17:25:53 +0800

    添加判断属性，方便使用

--- mt.js
+++ mt.js
@@ -4,35 +4,39 @@
 * 平板仅区分iPAD,其余平台归类为Mobile
 */
 
- var _PLATFORM = {
-   USERAGENT: navigator.userAgent.toLowerCase(),
-   DEVICETYPE: 'pc'
- };
+var _PLATFORM = {
+ USERAGENT: navigator.userAgent.toLowerCase(),
+ PCMARKER: 'pc',
+ PADMARKER: 'pad',
+ MOBILEMARKER: 'mobile'
+};
 
- if (/android|webos|ipad|iphone|ipod|blackberry|bb10|windows phone|samsung|kfthwi|kfapwi/i.test(_PLATFORM.USERAGENT)) {
-   if (/ipad/i.test(_PLATFORM.USERAGENT)) {
-     _PLATFORM.DEVICETYPE = "pad";
-   }else {
-     _PLATFORM.DEVICETYPE = "mobile";
-   }
+_PLATFORM.DEVICETYPE=_PLATFORM.PCMARKER;
+
+if (/android|webos|ipad|iphone|ipod|blackberry|bb10|windows phone|samsung|kfthwi|kfapwi/i.test(_PLATFORM.USERAGENT)) {
+ if (/ipad/i.test(_PLATFORM.USERAGENT)) {
+   _PLATFORM.DEVICETYPE = _PLATFORM.PADMARKER;
+ }else {
+   _PLATFORM.DEVICETYPE = _PLATFORM.MOBILEMARKER;
  }
+}
 
 _PLATFORM.isPc=function(){
-  if(this.DEVICETYPE==='pc'){
+  if(this.DEVICETYPE===this.PCMARKER){
     return true;
   }
   return false;
 }
 
 _PLATFORM.isPad=function(){
-  if(this.DEVICETYPE==='pad'){
+  if(this.DEVICETYPE===this.PADMARKER){
     return true;
   }
   return false;
 }
 
 _PLATFORM.isMobile=function(){
-  if(this.DEVICETYPE==='mobile'){
+  if(this.DEVICETYPE===this.MOBILEMARKER){
     return true;
   }
   return false;
